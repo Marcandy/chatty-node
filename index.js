f61e0ea29cd54bb63d6941248a934621aa16b422
marcandy <marcandy.nj@gmail.com>
2017-01-03 22:07:31 -0700

    master

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var express = require('express');
 
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.listen(3000);
