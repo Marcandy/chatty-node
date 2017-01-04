e72b46a21de0e10d9ecaa6c848df840411321acc
marcandy <marcandy.nj@gmail.com>
2017-01-03 23:33:39 -0700

    node

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ var app = express();
 app.use(express.static('assets'));
 app.use(bodyParser.json());
 
+var messages = [
+  {message: 'hello'},
+  {message: 'ma'}
+];
+
+app.get('/messages', function (req, res, next) {
+
+  res.status(200).json({messages: messages});
+})
+
+app.post('/messages', function (req, res, next) {
+
+  messages.push(req.body.message);// make sure it correspont to the property name
+  res.status(200).json({messages: messages});
+})
+
 app.listen(3000);
